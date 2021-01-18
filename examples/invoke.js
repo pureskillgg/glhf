707c26b89fb9d94f3e3040be7004b9ff010cd096
Evan Sosenko <evan@pureskill.gg>
2021-01-18 10:53:46 -0800

    Use functionName over arn

--- examples/invoke.js
+++ examples/invoke.js
@@ -4,7 +4,7 @@ export const red = ({ redLambdaArn, log }) => async (req = 'req') => {
   const input = await readJson('fixtures', `${req}.json`)
   const client = new LambdaClient({
     name: 'red',
-    arn: redLambdaArn,
+    functionName: redLambdaArn,
     log
   })
   return client.invokeJson(input)
