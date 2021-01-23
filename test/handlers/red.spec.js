03d63a6e2bc9053061088a36d7bad3287b95cace
Evan Sosenko <evan@pureskill.gg>
2021-01-22 22:30:57 -0800

    Update handler tests

--- test/handlers/red.spec.js
+++ test/handlers/red.spec.js
@@ -1,10 +1,23 @@
 import test from 'ava'
+import { asValue } from 'awilix'
 
 import { createHandleInvoke } from '../../handlers/red.js'
 
 test('invoke', async (t) => {
   const event = { foo: 'bar' }
-  const handler = createHandleInvoke()
-  const data = await handler(event)
-  t.snapshot(data)
+
+  const blueLambdaClient = {
+    invokeJson: async (...args) => {
+      t.snapshot(args, 'blueLambdaClient.invokeJson')
+      return { foo: 'baz' }
+    }
+  }
+
+  const overrideDependencies = (container) => {
+    container.register('blueLambdaClient', asValue(blueLambdaClient))
+  }
+
+  const handler = createHandleInvoke([], t, overrideDependencies)
+  const data = await handler(event, {})
+  t.snapshot(data, 'handler')
 })
