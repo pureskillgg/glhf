c307ccccce2cc15af10a792ad43c88199644b45d
Evan Sosenko <evan@pureskill.gg>
2021-04-21 17:40:21 -0700

    Add localParameters test for blue handler

--- handlers/blue.mjs
+++ handlers/blue.mjs
@@ -1,11 +1,22 @@
+import { asValue } from 'awilix'
 import { invokeHandler } from '../index.js'
+import { localString } from '@pureskillgg/ace'
 
-const createProcessor = () => async (event, container) => {
-  return event
+const registerDependencies = (container, config) => {
+  container.register({ rank: asValue(config.rank) })
+}
+
+const parameters = {
+  rank: localString('RANK')
+}
+
+const createProcessor = ({ rank }) => async (event, container) => {
+  return { ...event, rank }
 }
 
 export const createHandleInvoke = invokeHandler({
+  registerDependencies,
   createProcessor
 })
 
-export const handler = createHandleInvoke()
+export const handler = createHandleInvoke(parameters)
--- test/handlers/blue.spec.js
+++ test/handlers/blue.spec.js
@@ -1,10 +1,19 @@
 import test from 'ava'
+import { localString } from '@pureskillgg/ace'
 
 import { createHandleInvoke } from '../../handlers/blue.mjs'
 
 test('invoke', async (t) => {
   const event = { foo: 'bar' }
-  const handler = createHandleInvoke([], {}, t)
+  const handler = createHandleInvoke(parameters, localParameters, t)
   const data = await handler(event)
   t.snapshot(data, 'handler')
 })
+
+const parameters = {
+  rank: localString('rank')
+}
+
+const localParameters = {
+  rank: 'mock-rank'
+}
