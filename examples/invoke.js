8001e580e68ab312632ee63cf3d0ee4e78eb1419
Evan Sosenko <evan@pureskill.gg>
2021-05-11 16:51:29 -0700

    Run format

--- examples/invoke.js
+++ examples/invoke.js
@@ -2,12 +2,14 @@ import { LambdaClient } from '@pureskillgg/awsjs'
 
 import { readJson } from '../index.js'
 
-export const red = ({ redLambdaArn, log }) => async (req = 'req') => {
-  const input = await readJson('fixtures', `${req}.json`)
-  const client = new LambdaClient({
-    name: 'red',
-    functionName: redLambdaArn,
-    log
-  })
-  return client.invokeJson(input)
-}
+export const red =
+  ({ redLambdaArn, log }) =>
+  async (req = 'req') => {
+    const input = await readJson('fixtures', `${req}.json`)
+    const client = new LambdaClient({
+      name: 'red',
+      functionName: redLambdaArn,
+      log
+    })
+    return client.invokeJson(input)
+  }
