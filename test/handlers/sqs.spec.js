d5dab8117ca601c73a3717298c7386620d5ee549
Evan Sosenko <evan@pureskill.gg>
2021-07-13 12:40:45 -0700

    Snapshot event and context in test

--- test/handlers/sqs.spec.js
+++ test/handlers/sqs.spec.js
@@ -4,7 +4,7 @@ import { sqsHandler, sqsJsonHandler, readJson } from '../../index.js'
 
 test('sqsHandler: invoke', async (t) => {
   const event = await readJson('fixtures', 'event', 'sqs.json')
-  const createHandler = sqsHandler()
+  const createHandler = sqsHandler({ createProcessor })
   const handler = createHandler({}, t)
   const data = await handler(event)
   t.snapshot(data, 'handler')
@@ -12,8 +12,10 @@ test('sqsHandler: invoke', async (t) => {
 
 test('sqsJsonHandler: invoke', async (t) => {
   const event = await readJson('fixtures', 'event', 'sqs.json')
-  const createHandler = sqsJsonHandler()
+  const createHandler = sqsJsonHandler({ createProcessor })
   const handler = createHandler({}, t)
   const data = await handler(event)
   t.snapshot(data, 'handler')
 })
+
+const createProcessor = () => async (event, context) => ({ event, context })
