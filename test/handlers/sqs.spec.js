3df374f89da1b7eb90319b175ae5435c10d97de9
Evan Sosenko <evan@pureskill.gg>
2021-07-13 17:43:36 -0700

    SQS support (#6)
    * Add sqs parsers
    
    * Make sqs event fixture use JSON body
    
    * Export sqs parser
    
    * Parse number messageAttributes
    
    * Support Binary sqs messageAttributes
    
    * Add event type module
    
    * Add sqs type
    
    * Add recordsParser
    
    * Add spec for getRequestId
    
    * Run format
    
    * Export records parser
    
    * Add sqs request id support
    
    * Add missing docs
    
    * Do not pass context to discoverEventType
    
    * Add parallel strategy
    
    * Add invokeHandler doc
    
    * Export parallel strategy
    
    * Add sqs handlers
    
    * Run format
    
    * Add sqs handlers
    
    * Add records wrapper
    
    * Fix dummy params in test
    
    * Fix SQS handler
    
    * Snapshot event and context in test
    
    * Parse SQS attributes
    
    * Update log msg
    
    * Use destructed reqId
    
    * Update docs
    
    * Update docs
    
    * Update recordsParser
    
    * Pass full context into processor
    
    Co-authored-by: PureSkill.gg <secure@pureskill.gg>

---
+++ test/handlers/sqs.spec.js
@@ -0,0 +1,23 @@
+import test from 'ava'
+
+import { sqsHandler, sqsJsonHandler, readJson } from '../../index.js'
+
+test('sqsHandler: invoke', async (t) => {
+  const event = await readJson('fixtures', 'event', 'sqs.json')
+  event.reqId = 'mock-req-id'
+  const createHandler = sqsHandler({ createProcessor })
+  const handler = createHandler({}, t)
+  const data = await handler(event)
+  t.snapshot(data, 'handler')
+})
+
+test('sqsJsonHandler: invoke', async (t) => {
+  const event = await readJson('fixtures', 'event', 'sqs.json')
+  event.reqId = 'mock-req-id'
+  const createHandler = sqsJsonHandler({ createProcessor })
+  const handler = createHandler({}, t)
+  const data = await handler(event)
+  t.snapshot(data, 'handler')
+})
+
+const createProcessor = () => async (event, context) => ({ event, context })
